fc35edfd518881a615f83a38c860080c249fc0db
based64.eth <based64.eth@protonmail.com>
2025-02-08 11:37:31 +0800

    feat: add sidebar layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
 import "./App.css";
+import SidebarLayout from "./components/layout/SidebarLayout";
 import VideoEditor from "./components/VideoUploader";
 
 function App() {
   return (
-    <div className="flex flex-col items-end">
+    <SidebarLayout
+      slots={{
+        navigation: (
+          <div className="bg-gray-100 text-center h-full">Navigation</div>
+        ),
+        assetsDrawer: (
+          <div className="bg-gray-200 text-center h-full">Assets Drawer</div>
+        ),
+        rightSidebar: (
+          <div className="bg-gray-200 text-center h-full">Right Sidebar</div>
+        ),
+        topBar: <div className="bg-gray-100 text-center">Top Bar</div>,
+        bottomBar: <div className="bg-gray-100 text-center">Bottom Bar</div>,
+      }}
+    >
       <VideoEditor />
-    </div>
+    </SidebarLayout>
   );
 }
 
